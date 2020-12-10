d5209fd4454df213eff27ad31b704eba622f65c2
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2020-12-10 09:33:09 +0530

    Second_Apps

--- Intro_App/lib/main.dart
+++ Intro_App/lib/main.dart
@@ -5,12 +5,84 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        child: Column(
-          children: [Text("data"), Text("data")],
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/a.JPG'),
+              ),
+              Text(
+                'Akash Kumar Singh',
+                style: TextStyle(
+                  fontFamily: 'RussoOne',
+                  fontSize: 45.0,
+                  fontWeight: FontWeight.bold ,
+                  letterSpacing: 2.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  title: Text(
+                    '+91 8957564645',
+                    style: TextStyle(
+                        color: Colors.lightGreenAccent,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.0,
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  title: Text(
+                    'akashkumarsingh11032001@gmail.com',
+                    style: TextStyle(
+                        color: Colors.lightGreenAccent,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.0,
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
